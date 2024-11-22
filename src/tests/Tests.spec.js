6568588c060eefa381daf6104424c6148a4f894d
thiagoDimon <othiago.miranda@outlook.com>
2024-11-21 21:05:58 -0300

    first commit

---
+++ src/tests/Tests.spec.js
@@ -0,0 +1,50 @@
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import http from 'k6/http';
+import { check } from 'k6';
+import { Trend } from 'k6/metrics';
+
+export const getContactsDuration = new Trend('get_contacts', true);
+
+export const options = {
+  thresholds: {
+    http_req_failed: ['rate<0.01'],
+    http_req_duration: ['p(95)<300']
+  },
+  stages: [
+    { duration: '10s', target: 5 },
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 20 },
+    { duration: '30s', target: 30 },
+    { duration: '20s', target: 20 },
+    { duration: '10s', target: 10 },
+    { duration: '10s', target: 5 }
+  ]
+};
+
+export function handleSummary(data) {
+  return {
+    './src/output/index.html': htmlReport(data),
+    stdout: textSummary(data, { indent: ' ', enableColors: true })
+  };
+}
+
+export default function () {
+  const baseUrl = 'https://www.ateky.com.br/empresa/';
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const OK = 200;
+
+  const res = http.get(`${baseUrl}`, params);
+
+  getContactsDuration.add(res.timings.duration);
+
+  check(res, {
+    'GET Contacts - Status 200': () => res.status === OK
+  });
+}
